e66f1f8439cb85e07d2320bcdbe6e49345c88dad
Pina Colada <pina34colada@free.fr>
2017-12-10 17:01:11 +0100

    Couleur fond et bordure arrondie

--- renderer.js
+++ renderer.js
@@ -1,3 +1,48 @@
+class Couleur {
+    constructor(couleur, alpha = 1.0) {
+        this.obj = { r: 0, g: 0, b: 0, a: 1.0, txt: "#000000", val: 0, id: "" };
+        if (typeof couleur === "string") {
+            this.txt = couleur;
+        }
+        else {
+            this.val = couleur;
+        }
+        this.obj.a = alpha;
+    }
+    get rgb() {
+        return `rgb(${this.obj.r}, ${this.obj.g}, ${this.obj.b})`;
+    }
+    get rgba() {
+        return `rgba(${this.obj.r}, ${this.obj.g}, ${this.obj.b}, ${this.obj.a})`;
+    }
+    get txt() {
+        return this.obj.txt;
+    }
+    set txt(couleur) {
+        if (couleur.indexOf("#") === 0) {
+            this.val = parseInt(couleur.substr(1), 16);
+        }
+        else if (couleur.indexOf("rgba(") === 0) {
+            let rgb = couleur.split("(")[1];
+            [this.obj.r, this.obj.g, this.obj.b, this.obj.a] = rgb.split(",").map(item => parseFloat(item));
+        }
+        else if (couleur.indexOf("rgb(") === 0) {
+            let rgb = couleur.split("(")[1];
+            [this.obj.r, this.obj.g, this.obj.b] = rgb.split(",").map(item => parseInt(item, 10));
+        }
+        else {
+            this.obj.txt = couleur; // nom de la couleur
+        }
+    }
+    get val() {
+        return this.obj.val;
+    }
+    set val(couleur) {
+        this.obj.val = couleur;
+        // tslint:disable-next-line:no-bitwise
+        this.obj.r = (couleur >> 16) & 0xFF, this.obj.g = (couleur >> 8) & 0xFF, this.obj.g = couleur & 0xFF;
+    }
+}
 class Point {
     constructor(x = 0, y = 0) {
         this.x = x;
@@ -39,6 +84,15 @@ class Point {
     }
     static Polar(p1, distance, angleRad) {
         return new Point(p1.x + Math.cos(angleRad) * distance, p1.y + Math.sin(angleRad) * distance);
+    }
+}
+class HTMLVisuel {
+    constructor(type) {
+        this.el = document.createElement(type);
+        this.el["visuel"] = this;
+    }
+    dispatch(eventType) {
+        this.el.dispatchEvent(new Event(eventType));
     }
 }
 class Rect extends Point {
@@ -110,17 +164,13 @@ class Rect extends Point {
         this.right = value.x;
     }
 }
-class Visuel {
+class Visuel extends HTMLVisuel {
     constructor(type = "DIV", id, pos) {
+        super(type);
         this.children = [];
         this._pos = new Point();
-        this.el = document.createElement(type);
-        this.el["visuel"] = this;
         this.pos = new Point(pos.x, pos.y);
         this.setCss("box-sizing", "border-box", "position", "absolute");
-    }
-    dispatch(eventType) {
-        this.el.dispatchEvent(new Event(eventType));
     }
     get pos() {
         return this._pos;
@@ -131,6 +181,18 @@ class Visuel {
         }
         this._pos.setTo(value.x, value.y);
         this.setCss("left", `${this._pos.x}px`, "top", `${this._pos.y}px`);
+    }
+    get backgroundColor() {
+        return new Couleur(this.el.style.backgroundColor).val;
+    }
+    set backgroundColor(couleur) {
+        this.setCss("background-color", new Couleur(couleur).rgb);
+    }
+    get borderColor() {
+        return new Couleur(this.el.style.borderColor).val;
+    }
+    set borderColor(couleur) {
+        this.setCss("border", "1px solid " + new Couleur(couleur).rgb);
     }
     get x() {
         return this._pos.x;
@@ -152,18 +214,20 @@ class Visuel {
             this.dispatch("position");
         }
     }
-    setCss(...propVals) {
-        for (let i = 0; i < propVals.length; i += 2) {
-            let [prop, val] = [propVals[i], propVals[i + 1]];
-            this.el.style[prop] = val;
-        }
-    }
     addChild(v) {
         if (v === null) {
             throw new TypeError("Impossible d'ajouter un visuel nul");
         }
         v.parent = this; // tout faire dans le setter parent
         return v;
+    }
+    addTo(alt) {
+        if (alt instanceof Visuel) {
+            this.parent = alt;
+        }
+        else if (alt instanceof HTMLElement) {
+            alt.appendChild(this.el);
+        }
     }
     removeChild(v) {
         if (this.children.indexOf(v) > -1) {
@@ -176,39 +240,37 @@ class Visuel {
             this.children.pop().parent = null;
         }
     }
+    setCss(...propVals) {
+        for (let i = 0; i < propVals.length; i += 2) {
+            let [prop, val] = [propVals[i], propVals[i + 1]];
+            this.el.style[prop] = val;
+        }
+    }
     get numChildren() {
         return this.children.length;
     }
-    get parentEl() {
-        return this.el.parentElement;
-    }
-    set parentEl(value) {
-        if (value !== null) {
-            value.appendChild(this.el);
-            let p = value["visuel"];
-            if (p !== null && p.children.indexOf(this) === -1) {
-                p.children.push(this);
-            }
-        }
-    }
     get parent() {
-        return this.parentEl["visuel"];
+        return this.el.parentElement["visuel"];
     }
     set parent(value) {
         if (value === null) {
-            // on enlève le visuel de son support
-            let pere = this.el.parentElement;
-            if (pere !== null) {
-                let pv = pere["visuel"];
-                pere.removeChild(this.el);
-                if (pv != null) {
-                    let i = pv.children.indexOf(this);
-                    pv.children.splice(i, 1);
+            // on enlève d'abord ce visuel de son support si c'est un visuel
+            if (this.el.parentElement !== null) {
+                this.el.parentElement.removeChild(this.el);
+                if (this.el.parentElement["visuel"] instanceof Visuel) {
+                    let pv = this.el.parentElement["visuel"];
+                    pv.children.splice(pv.children.indexOf(this), 1);
                 }
             }
+            else if (this.el.parentNode != null) {
+                this.el.parentNode.removeChild(this.el);
+            }
         }
-        else if (value !== this.parent) {
-            this.parentEl = value.el;
+        else if (value instanceof Visuel) {
+            value.el.appendChild(this.el);
+            if (value.children.indexOf(this) == -1) {
+                value.children.push(this);
+            }
         }
     }
 }
@@ -242,8 +304,8 @@ class Ligne extends Visuel {
 }
 let r = new Rect(100, 50, 400, 250);
 let cadre = new Frame("cadre", r);
-cadre.setCss("background-color", "#999999");
-document.body.appendChild(cadre.el);
+cadre.backgroundColor = 0x999999;
+cadre.addTo(document.body);
 let dia_1 = new Ligne("dia_1", r.topLeft, r.botRight).setStyle("red");
 let dia_2 = new Ligne("dia_2", r.botLeft, r.topRight).setStyle("green");
 let haut = new Ligne("haut", r.topLeft, r.topRight).setStyle("blue");
@@ -255,3 +317,5 @@ let hCent = hRect.topLeft.offset(hRect.width / 2, hRect.height / 2);
 console.log("l'horloge est placée en", hRect.toString());
 console.log("Son centre est en", hCent.toString());
 let horloge = new Frame("horloge", hRect);
+horloge.backgroundColor = 0x6666FF;
+horloge.addTo(document.body);
--- renderer.ts
+++ renderer.ts
@@ -1,3 +1,45 @@
+class Couleur {
+    obj =  {  r:0,  g:0,  b:0,  a:1.0,   txt:"#000000", val:0, id:""};
+    constructor(couleur:string|number, alpha:number = 1.0) {
+        if(typeof couleur === "string") {
+            this.txt = couleur;
+        } else {
+            this.val = couleur;
+        }
+        this.obj.a = alpha;
+    }
+    get rgb():string {
+        return `rgb(${this.obj.r}, ${this.obj.g}, ${this.obj.b})`;
+    }
+    get rgba():string {
+        return `rgba(${this.obj.r}, ${this.obj.g}, ${this.obj.b}, ${this.obj.a})`;
+    }
+    get txt():string {
+        return this.obj.txt;
+    }
+    set txt(couleur:string) {
+        if(couleur.indexOf("#")===0) {
+            this.val = parseInt(couleur.substr(1), 16);
+        } else if(couleur.indexOf("rgba(")===0) {
+            let rgb:string = couleur.split("(")[1];
+            [this.obj.r, this.obj.g, this.obj.b, this.obj.a] = rgb.split(",").map(item => parseFloat(item));
+        } else if(couleur.indexOf("rgb(")===0) {
+            let rgb:string = couleur.split("(")[1];
+            [this.obj.r, this.obj.g, this.obj.b] = rgb.split(",").map(item => parseInt(item,10));
+        } else {
+            this.obj.txt = couleur; // nom de la couleur
+        }
+    }
+    get val():number {
+        return this.obj.val;
+    }
+
+    set val(couleur:number) {
+        this.obj.val = couleur;
+        // tslint:disable-next-line:no-bitwise
+        this.obj.r = (couleur >> 16) & 0xFF, this.obj.g = (couleur >> 8) & 0xFF, this.obj.g = couleur & 0xFF;
+    }
+}
 class Point {
     constructor(public x:number=0, public y:number=0) {
     }
@@ -108,8 +150,8 @@ class Rect extends Point {
     }
 }
 class Visuel {
-    el:HTMLElement;
     children:Visuel[] = [];
+    el:HTMLElement;
     _pos:Point = new Point();
     constructor(type:string="DIV", id:string, pos:Point) {
         this.el = document.createElement(type);
@@ -117,8 +159,57 @@ class Visuel {
         this.pos = new Point(pos.x, pos.y);
         this.setCss("box-sizing", "border-box", "position","absolute");
     }
+    addChild (v:Visuel):Visuel {
+        if(v === null) {
+            throw new TypeError("Impossible d'ajouter un visuel nul");
+        }
+        v.parent = this;// tout faire dans le setter parent
+        return v;
+    }
+    addTo(alt:any):void {
+        if(alt instanceof Visuel) {
+            this.parent = alt;
+        } else if (alt instanceof HTMLElement) {
+            alt.appendChild(this.el);
+        }
+    }
     dispatch(eventType:string):void {
         this.el.dispatchEvent(new Event(eventType));
+    }
+    removeChild(v:Visuel):Visuel {
+        if(this.children.indexOf(v)> -1) {
+            v.parent = null;
+        }
+        return v;
+    }
+    removeChildren():void {
+        while(this.numChildren) {
+            this.children.pop().parent = null;
+        }
+    }
+    setCss (...propVals:string[]):void {
+        for(let i:number = 0; i < propVals.length; i+=2) {
+            let [prop, val] = [propVals[i],propVals[i+1]];
+            this.el.style[prop] = val;
+        }
+    }
+    get backgroundColor():number {
+        return new Couleur(this.el.style.backgroundColor).val;
+    }
+    set backgroundColor(couleur:number) {
+        this.setCss("background-color", new Couleur(couleur).rgb);
+    }
+    get borderColor():number {
+        return new Couleur(this.el.style.borderColor).val;
+    }
+    set borderColor(couleur:number) {
+        this.setCss("border", "1px solid "+new Couleur(couleur).rgb);
+    }
+    get borderRadius():number {
+        return parseInt(this.el.style.borderRadius, 10);
+    }
+    set borderRadius(value:number) {
+        this.setCss("border-radius", `${value}px`);
     }
     get pos():Point {
         return this._pos;
@@ -150,62 +241,30 @@ class Visuel {
             this.dispatch("position");
         }
     }
-    setCss (...propVals:string[]):void {
-        for(let i:number = 0; i < propVals.length; i+=2) {
-            let [prop, val] = [propVals[i],propVals[i+1]];
-            this.el.style[prop] = val;
-        }
-    }
-    addChild (v:Visuel):Visuel {
-        if(v === null) {
-            throw new TypeError("Impossible d'ajouter un visuel nul");
-        }
-        v.parent = this;// tout faire dans le setter parent
-        return v;
-    }
-    removeChild(v:Visuel):Visuel {
-        if(this.children.indexOf(v)> -1) {
-            v.parent = null;
-        }
-        return v;
-    }
-    removeChildren():void {
-        while(this.numChildren) {
-            this.children.pop().parent = null;
-        }
-    }
     get numChildren():number {
         return this.children.length;
     }
-    get parentEl():HTMLElement {
-        return this.el.parentElement;
-    }
-    set parentEl(value:HTMLElement) {
-        if(value !== null) {
-            value.appendChild(this.el);
-            let p:Visuel = value["visuel"];
-            if(p !== null && p.children.indexOf(this) === -1) {
-                p.children.push(this);
-            }
-        }
-    }
+
     get parent():Visuel {
-        return this.parentEl["visuel"];
+        return this.el.parentElement["visuel"];
     }
     set parent(value:Visuel) {
         if(value === null) {
-            // on enlève le visuel de son support
-            let pere:HTMLElement = this.el.parentElement;
-            if(pere !== null) {
-                let pv:Visuel = pere["visuel"];
-                pere.removeChild(this.el);
-                if(pv != null) {
-                    let i:number = pv.children.indexOf(this);
-                    pv.children.splice(i, 1);
+            // on enlève d'abord ce visuel de son support si c'est un visuel
+            if(this.el.parentElement !== null) {
+                this.el.parentElement.removeChild(this.el);
+                if(this.el.parentElement["visuel"] instanceof Visuel) {
+                   let pv:Visuel = this.el.parentElement["visuel"];
+                   pv.children.splice(pv.children.indexOf(this), 1);
                 }
+            } else if(this.el.parentNode !=null) {
+                this.el.parentNode.removeChild(this.el);
             }
-        } else if (value !== this.parent) {// on n'ajoute pas un enfant déjà mis
-            this.parentEl = value.el;
+        } else if (value instanceof Visuel) {
+            value.el.appendChild(this.el);
+            if (value.children.indexOf ( this ) === -1) {
+                value.children.push(this);
+            }
         }
     }
 }
@@ -245,8 +304,8 @@ class Ligne extends Visuel {
 let r:Rect = new Rect(100, 50, 400, 250);
 
 let cadre:Frame = new Frame("cadre", r);
-cadre.setCss("background-color", "#999999");
-document.body.appendChild(cadre.el);
+cadre.backgroundColor = 0x999999;
+cadre.addTo(document.body);
 
 let dia_1:Ligne = new Ligne("dia_1", r.topLeft,  r.botRight).setStyle("red");
 let dia_2:Ligne = new Ligne("dia_2", r.botLeft,  r.topRight).setStyle("green");
@@ -261,3 +320,6 @@ console.log("l'horloge est placée en", hRect.toString());
 console.log("Son centre est en", hCent.toString());
 
 let horloge:Frame = new Frame("horloge", hRect);
+horloge.backgroundColor = 0x66FF66;
+horloge.borderRadius = 20;
+horloge.addTo(document.body);
